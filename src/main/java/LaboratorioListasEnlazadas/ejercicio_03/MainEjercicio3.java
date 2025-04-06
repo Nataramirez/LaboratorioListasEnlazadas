7ddc9e28ecb119194ef506a011c97668ea3402c5
jhainethv161 <vnaranjo@sodimac.vc-soft.com>
2025-04-06 17:14:16 -0500

    Ejercicios 3, 6

---
+++ src/main/java/LaboratorioListasEnlazadas/ejercicio_03/MainEjercicio3.java
@@ -0,0 +1,45 @@
+package LaboratorioListasEnlazadas.ejercicio_03;
+
+import LaboratorioListasEnlazadas.Servicios.ListaEnlazadaSimple;
+
+import java.util.Iterator;
+
+public class MainEjercicio3 {
+    public static void main(String[] args) {
+
+        ListaEnlazadaSimple<Integer> listaNumeros = new ListaEnlazadaSimple<>();
+
+        listaNumeros.agregarAlInicio(2);
+        listaNumeros.agregarAlInicio(3);
+        listaNumeros.agregarAlInicio(8);
+        listaNumeros.agregarAlInicio(15);
+        listaNumeros.agregarAlInicio(23);
+        listaNumeros.agregarAlInicio(26);
+
+        listaNumeros.imprimir();
+
+        System.out.println("................");
+
+        eliminarParesLista(listaNumeros);
+
+        listaNumeros.imprimir();
+
+    }
+
+    public static void eliminarParesLista(ListaEnlazadaSimple<Integer> listaEnlazadaSimple) {
+
+        for (Integer valor : listaEnlazadaSimple){
+
+        }
+
+        Iterator<Integer> iterator = listaEnlazadaSimple.iterator();
+
+        while (iterator.hasNext()) {
+
+            int valor = iterator.next();
+            if (valor % 2 == 0) {
+                listaEnlazadaSimple.eliminarNodo(valor);
+            }
+        }
+    }
+}
---
+++ src/main/java/LaboratorioListasEnlazadas/ejercicio_06/MainEjercicio6.java
@@ -0,0 +1,17 @@
+package LaboratorioListasEnlazadas.ejercicio_06;
+
+import LaboratorioListasEnlazadas.Servicios.ListaEnlazadaDoble;
+
+public class MainEjercicio6 {
+    public static void main(String[] args) {
+
+        ListaEnlazadaDoble<String> listaNombres =  new ListaEnlazadaDoble<>();
+
+        listaNombres.agregarAlFinal("Nata");
+        listaNombres.agregarAlFinal("Sara");
+        listaNombres.agregarAlFinal("Valen");
+        listaNombres.agregarAlInicio("Robinson");
+
+        listaNombres.imprimirHaciaAtras();
+    }
+}
